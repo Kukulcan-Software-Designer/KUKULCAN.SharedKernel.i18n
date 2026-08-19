29b8e9faad1edfbc05bab558bbf7164aca9576fb
Juan Manuel Pardo García <jpardo.kukulcan@gmail.com>
2026-08-20 00:41:46 +0200

    test: add LanguageCode NUnit coverage

---
+++ Tests/KUKULCAN.SharedKernel.i18n.Domain.UnitTests/ValueObjects/LanguageCodeTests.cs
@@ -0,0 +1,94 @@
+using KUKULCAN.SharedKernel.i18n.Domain.ValueObjects;
+
+namespace KUKULCAN.SharedKernel.i18n.Domain.UnitTests.ValueObjects;
+
+[TestFixture]
+public sealed class LanguageCodeTests
+{
+    [TestCase("es-ES", "es-ES", "es", "ES")]
+    [TestCase("ES-es", "es-ES", "es", "ES")]
+    [TestCase("en", "en", "en", null)]
+    [TestCase("ca-ES", "ca-ES", "ca", "ES")]
+    public void Create_ValidTag_NormalisesAndParses(string input, string value, string language, string? region)
+    {
+        var result = LanguageCode.Create(input);
+
+        Assert.That(result.IsSuccess, Is.True);
+        Assert.That(result.Value.Value, Is.EqualTo(value));
+        Assert.That(result.Value.Language, Is.EqualTo(language));
+        Assert.That(result.Value.Region, Is.EqualTo(region));
+        Assert.That(result.Value.ToString(), Is.EqualTo(value));
+        Assert.That((string)result.Value, Is.EqualTo(value));
+    }
+
+    [TestCase(null)]
+    [TestCase("")]
+    [TestCase("   ")]
+    public void Create_EmptyTag_ReturnsFailure(string? input)
+    {
+        var result = LanguageCode.Create(input);
+
+        Assert.That(result.IsFailure, Is.True);
+    }
+
+    [TestCase("e")]
+    [TestCase("english")]
+    [TestCase("es_")]
+    [TestCase("es--ES")]
+    [TestCase("es-123456789")]
+    public void Create_InvalidTag_ReturnsFailure(string input)
+    {
+        var result = LanguageCode.Create(input);
+
+        Assert.That(result.IsFailure, Is.True);
+    }
+
+    [Test]
+    public void FallbackChain_RegionalLanguage_ReturnsExactLanguageThenParentThenEnglish()
+    {
+        var result = LanguageCode.Create("es-MX");
+
+        Assert.That(result.IsSuccess, Is.True);
+        Assert.That(result.Value.FallbackChain, Is.EqualTo(new[] { "es-MX", "es", "en" }));
+    }
+
+    [Test]
+    public void FallbackChain_LanguageOnly_DoesNotDuplicateEnglish()
+    {
+        var result = LanguageCode.Create("en");
+
+        Assert.That(result.IsSuccess, Is.True);
+        Assert.That(result.Value.FallbackChain, Is.EqualTo(new[] { "en" }));
+    }
+
+    [Test]
+    public void FallbackChain_IsReadOnlyAndStableAcrossReads()
+    {
+        var result = LanguageCode.Create("fr-FR");
+
+        var first = result.Value.FallbackChain;
+        var second = result.Value.FallbackChain;
+
+        Assert.That(first, Is.EqualTo(second));
+        Assert.That(first, Is.Not.SameAs(second));
+    }
+
+    [TestCase("zh-Hant-TW")]
+    [TestCase("sr-Latn-RS")]
+    public void Create_ComplexBcp47Tag_IsAccepted(string input)
+    {
+        var result = LanguageCode.Create(input);
+
+        Assert.That(result.IsSuccess, Is.True);
+    }
+
+    [Test]
+    public void Equality_IsBasedOnNormalisedValue()
+    {
+        var first = LanguageCode.Create("ES-es").Value;
+        var second = LanguageCode.Create("es-ES").Value;
+
+        Assert.That(first, Is.EqualTo(second));
+        Assert.That(first.GetHashCode(), Is.EqualTo(second.GetHashCode()));
+    }
+}
