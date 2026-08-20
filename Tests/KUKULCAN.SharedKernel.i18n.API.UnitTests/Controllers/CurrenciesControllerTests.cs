6ff2235a0030613b1a33b086ae6bf39c3c7e081b
Juan Manuel Pardo García <jpardo.kukulcan@gmail.com>
2026-08-20 13:48:33 +0200

    Corrección del error de falta de un USING en pruebas unitarias de la API

--- Tests/KUKULCAN.SharedKernel.i18n.API.UnitTests/Controllers/CurrenciesControllerTests.cs
+++ Tests/KUKULCAN.SharedKernel.i18n.API.UnitTests/Controllers/CurrenciesControllerTests.cs
@@ -1,6 +1,9 @@
+using System.Reflection;
 using KUKULCAN.SharedKernel.i18n.API.Controllers;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
 
 namespace KUKULCAN.SharedKernel.i18n.API.UnitTests.Controllers;
 
@@ -10,9 +13,9 @@ public sealed class CurrenciesControllerTests
     [Test]
     public void Controller_HasExpectedRouteAndProducesMetadata()
     {
-        var type = typeof(CurrenciesController);
-        var route = type.GetCustomAttributes(typeof(RouteAttribute), inherit: true).Single();
-        var produces = type.GetCustomAttributes(typeof(ProducesAttribute), inherit: true).Single();
+        Type type = typeof(CurrenciesController);
+        object route = type.GetCustomAttributes(typeof(RouteAttribute), inherit: true).Single();
+        object produces = type.GetCustomAttributes(typeof(ProducesAttribute), inherit: true).Single();
 
         Assert.That(((RouteAttribute)route).Template, Is.EqualTo("api/v1/currencies/{languageCode}"));
         Assert.That(((ProducesAttribute)produces).ContentTypes, Does.Contain("application/json"));
@@ -21,10 +24,10 @@ public sealed class CurrenciesControllerTests
     [Test]
     public void Upsert_HasExpectedHttpRouteAuthorizationAndResponseMetadata()
     {
-        var method = typeof(CurrenciesController).GetMethod(nameof(CurrenciesController.Upsert))!;
-        var httpPut = method.GetCustomAttributes(typeof(HttpPutAttribute), inherit: true).Single();
-        var authorize = method.GetCustomAttributes(typeof(AuthorizeAttribute), inherit: true).Single();
-        var responses = method.GetCustomAttributes(typeof(ProducesResponseTypeAttribute), inherit: true)
+        MethodInfo method = typeof(CurrenciesController).GetMethod(nameof(CurrenciesController.Upsert))!;
+        object httpPut = method.GetCustomAttributes(typeof(HttpPutAttribute), inherit: true).Single();
+        object authorize = method.GetCustomAttributes(typeof(AuthorizeAttribute), inherit: true).Single();
+        ProducesResponseTypeAttribute[] responses = method.GetCustomAttributes(typeof(ProducesResponseTypeAttribute), inherit: true)
             .Cast<ProducesResponseTypeAttribute>()
             .ToArray();
 
@@ -37,10 +40,10 @@ public sealed class CurrenciesControllerTests
     [Test]
     public void Delete_HasExpectedHttpRouteAuthorizationAndResponseMetadata()
     {
-        var method = typeof(CurrenciesController).GetMethod(nameof(CurrenciesController.Delete))!;
-        var httpDelete = method.GetCustomAttributes(typeof(HttpDeleteAttribute), inherit: true).Single();
-        var authorize = method.GetCustomAttributes(typeof(AuthorizeAttribute), inherit: true).Single();
-        var response = method.GetCustomAttributes(typeof(ProducesResponseTypeAttribute), inherit: true).Single();
+        MethodInfo method = typeof(CurrenciesController).GetMethod(nameof(CurrenciesController.Delete))!;
+        object httpDelete = method.GetCustomAttributes(typeof(HttpDeleteAttribute), inherit: true).Single();
+        object authorize = method.GetCustomAttributes(typeof(AuthorizeAttribute), inherit: true).Single();
+        object response = method.GetCustomAttributes(typeof(ProducesResponseTypeAttribute), inherit: true).Single();
 
         Assert.That(((HttpDeleteAttribute)httpDelete).Template, Is.EqualTo("{currencyCode}"));
         Assert.That(((AuthorizeAttribute)authorize).Policy, Is.EqualTo("i18n.write"));
@@ -50,10 +53,10 @@ public sealed class CurrenciesControllerTests
     [Test]
     public void GetByLanguage_HasExpectedHttpRouteAuthorizationAndResponseMetadata()
     {
-        var method = typeof(CurrenciesController).GetMethod(nameof(CurrenciesController.GetByLanguage))!;
-        var httpGet = method.GetCustomAttributes(typeof(HttpGetAttribute), inherit: true).Single();
-        var authorize = method.GetCustomAttributes(typeof(AuthorizeAttribute), inherit: true).Single();
-        var response = method.GetCustomAttributes(typeof(ProducesResponseTypeAttribute), inherit: true).Single();
+        MethodInfo method = typeof(CurrenciesController).GetMethod(nameof(CurrenciesController.GetByLanguage))!;
+        object httpGet = method.GetCustomAttributes(typeof(HttpGetAttribute), inherit: true).Single();
+        object authorize = method.GetCustomAttributes(typeof(AuthorizeAttribute), inherit: true).Single();
+        object response = method.GetCustomAttributes(typeof(ProducesResponseTypeAttribute), inherit: true).Single();
 
         Assert.That(((HttpGetAttribute)httpGet).Template, Is.EqualTo(""));
         Assert.That(((AuthorizeAttribute)authorize).Policy, Is.EqualTo("i18n.read"));
