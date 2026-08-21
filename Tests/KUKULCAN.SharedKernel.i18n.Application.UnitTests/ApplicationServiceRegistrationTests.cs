1921a5ce45d5e5482771520ede573f72fe8cee48
Juan Manuel Pardo García <jpardo.kukulcan@gmail.com>
2026-08-21 19:03:06 +0200

    test: provide translation repository dependency in DI registration test

--- Tests/KUKULCAN.SharedKernel.i18n.Application.UnitTests/ApplicationServiceRegistrationTests.cs
+++ Tests/KUKULCAN.SharedKernel.i18n.Application.UnitTests/ApplicationServiceRegistrationTests.cs
@@ -1,10 +1,12 @@
 using KUKULCAN.SharedKernel.i18n.Application.Behaviors;
 using KUKULCAN.SharedKernel.i18n.Application.Features.Languages.Commands.CreateLanguage;
 using KUKULCAN.SharedKernel.i18n.Domain.DTOs;
+using KUKULCAN.SharedKernel.i18n.Domain.Interfaces.Repositories;
 using KUKULCAN.SharedKernel.i18n.Domain.Interfaces.Services;
 using KUKULCAN.SharedKernel.Results;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using Moq;
 
 namespace KUKULCAN.SharedKernel.i18n.Application.UnitTests;
 
@@ -15,6 +17,7 @@ public sealed class ApplicationServiceRegistrationTests
     public void AddKukulcanI18NApplication_RegistersApplicationServicesAndPipelineBehaviors()
     {
         var services = new ServiceCollection();
+        services.AddSingleton(new Mock<ITranslationRepository>().Object);
         IServiceCollection returned = services.AddKukulcanI18NApplication();
 
         Assert.That(returned, Is.SameAs(services));
