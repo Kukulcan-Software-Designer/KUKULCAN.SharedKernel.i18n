480795192ed01657a933f33bb33d1f9d152ffece
Juan Manuel Pardo García <jpardo.kukulcan@gmail.com>
2026-08-23 16:36:21 +0200

    Nueva migracion añadida

---
+++ Source/KUKULCAN.SharedKernel.i18n.Infrastructure/Migrations/20260823143457_EnsureSingleDefaultLanguage.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KUKULCAN.SharedKernel.i18n.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class EnsureSingleDefaultLanguage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
